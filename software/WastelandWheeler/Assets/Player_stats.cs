e1276eda93f6cc72731e31006585dec68d714141
AJ <aj.nargang@gmail.com>
2019-10-01 13:12:12 -0600

    Stats Script
    Simple set of stats that can be changed with picking up the power-ups.

---
+++ software/WastelandWheeler/Assets/Player_stats.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_stats : MonoBehaviour
+{
+    public float health = 100f;
+    public float rate_of_fire = 5f;
+    public float bullet_size = 2f;
+}
