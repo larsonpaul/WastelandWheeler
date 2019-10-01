44d16430b8fa8f771e1a62db823a098c71082307
AJ <aj.nargang@gmail.com>
2019-10-01 13:11:16 -0600

    Shooting Scripts
    One script deals with the player's shooting rate and the  spawning of bullets and the other destroys the bullets upon collision with colliders.

---
+++ software/WastelandWheeler/Assets/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    //public GameObject hit_effect;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //GameObject effect = Instantiate(hit_effect, transform.position, Quaternion.identity);
+        //Destroy(effect, 5f);
+        Destroy(gameObject);
+    }
+}
---
+++ software/WastelandWheeler/Assets/Shooting.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform fire_point;
+    public GameObject bulletPreFab;
+    public float bullet_force = 20f;
+    public float fire_rate = 2f;
+    public float fire_timer = 0.2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            if (fire_timer == 0)
+            {
+                Shoot();
+                //ROF_up fast_rof = gameObject.GetComponent<ROF_up>();
+                //fire_timer = fast_rof.new_rof;
+                fire_timer = 0.2f;
+            }
+            else
+            {
+                fire_timer -= Time.deltaTime;
+            }
+            if (fire_timer < 0)
+            {
+                fire_timer = 0f;
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPreFab, fire_point.position, fire_point.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(fire_point.up * bullet_force, ForceMode2D.Impulse);
+    }
+}
