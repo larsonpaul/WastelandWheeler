b850f570ccaa53242ca32bae3ae3f2b1d9f08976
AJ <aj.nargang@gmail.com>
2019-10-01 13:13:31 -0600

    Main Menu Script
    Will allow is to go to each game scene.

---
+++ software/WastelandWheeler/Assets/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void Play_game()
+    {
+        if (gameObject.CompareTag("Top_down"))
+        {
+            SceneManager.LoadScene(1);
+        }
+        if (gameObject.CompareTag("Platformer"))
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
