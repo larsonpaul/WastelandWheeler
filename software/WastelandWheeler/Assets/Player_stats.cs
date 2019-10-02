2b7bac7aa87e12470727110df46ba7297c97e8e5
Lasrrr <larsonpaul@live.ca>
2019-10-02 13:00:46 -0600

    Moved player movespeed to player_stats

--- software/WastelandWheeler/Assets/Player_stats.cs
+++ software/WastelandWheeler/Assets/Player_stats.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player_stats : MonoBehaviour
 {
     public float health = 100f;
-    public float rate_of_fire = 5f;
+    public float move_speed = 10f;
+    public float rate_of_fire = 0.2f;
     public float bullet_size = 2f;
 }
--- software/WastelandWheeler/Assets/Speed_up.cs
+++ software/WastelandWheeler/Assets/Speed_up.cs
@@ -7,6 +7,13 @@ public class Speed_up : MonoBehaviour
     public float multiplier = 1.5f;
     public float duration = 10f;
 
+    private static Player_stats stats;
+
+    void Start()
+    {
+        stats = GameObject.FindWithTag("Player").GetComponent<Player_stats>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
@@ -17,16 +24,14 @@ public class Speed_up : MonoBehaviour
 
     IEnumerator Pickup(Collider2D player)
     {
-        player_movement move_speed = player.GetComponent<player_movement>();
-
-        move_speed.move_speed *= multiplier;
+        stats.move_speed *= multiplier;
 
         GetComponent<SpriteRenderer>().enabled = false;
         GetComponent<PolygonCollider2D>().enabled = false;
 
         yield return new WaitForSeconds(duration);
 
-        move_speed.move_speed /= multiplier;
+        stats.move_speed /= multiplier;
 
         Destroy(gameObject);
     }
