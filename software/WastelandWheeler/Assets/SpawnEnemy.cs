5a05479a7debc929259dbf7f4b3f51e1eab60994
Lasrrr <larsonpaul@live.ca>
2019-10-01 23:57:51 -0600

    Summon any prefab with a right-click

---
+++ software/WastelandWheeler/Assets/SpawnEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SpawnEnemy : MonoBehaviour
+{
+
+    public GameObject prefab;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Vector3 pos = Input.mousePosition; 
+            pos = Camera.main.ScreenToWorldPoint(pos);
+            pos.z = 0f;
+            GameObject enemy = Instantiate(prefab, pos, Quaternion.identity);
+        }
+    }
+
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Click!");
+        if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            Debug.Log("Right Click!");
+            GameObject enemy = Instantiate(prefab, Camera.main.ViewportToWorldPoint(Input.mousePosition), Quaternion.identity);
+        }
+    }
+
+}
