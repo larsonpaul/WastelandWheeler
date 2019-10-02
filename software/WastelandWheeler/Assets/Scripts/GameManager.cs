2596132b668d303c262565c4156e3b0015ef36a5
Lasrrr <larsonpaul@live.ca>
2019-10-02 16:54:02 -0600

    Added GameManager object and script for game-over handling

---
+++ software/WastelandWheeler/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public float endDelay = 3f;
+
+    public void EndGame()
+    {
+        Debug.Log("GAME OVER!");
+        Invoke("Menu", endDelay);
+    }
+
+    void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
