004aacd0d59b8c3f47e47eda75766c07a2752a94
Lasrrr <larsonpaul@live.ca>
2019-10-02 16:56:46 -0600

    Enemies now do 10 damage and bounce off player

--- software/WastelandWheeler/Assets/Scripts/EnemyDamage.cs
+++ software/WastelandWheeler/Assets/Scripts/EnemyDamage.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class EnemyDamage : MonoBehaviour
 {
+    Rigidbody2D rbody;
+
+    public float damage = -10f;
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+
     void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.CompareTag("Player"))
         {
-            Destroy(col.gameObject);
+            rbody.velocity *= -2;
+            col.gameObject.GetComponent<Player_stats>().addHealth(damage);
         }
     }
 }
