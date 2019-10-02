e236f0eaba3ab865e972ecdb898226ebd3364354
Lasrrr <larsonpaul@live.ca>
2019-10-02 16:56:18 -0600

    Added functions to modify health and get stats

--- software/WastelandWheeler/Assets/Scripts/Player_stats.cs
+++ software/WastelandWheeler/Assets/Scripts/Player_stats.cs
@@ -8,4 +8,44 @@ public class Player_stats : MonoBehaviour
     public float move_speed = 10f;
     public float rate_of_fire = 0.2f;
     public float bullet_size = 2f;
+
+    public bool isInvincible = false;
+
+
+    public float getHealth()
+    {
+        return health;
+    }
+
+    public void addHealth(float num)
+    {
+        if (num < 0 && isInvincible) return;
+
+        health += num;
+        if (health <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    public float getSpeed()
+    {
+        return move_speed;
+    }
+
+    public float getROF()
+    {
+        return rate_of_fire;
+    }
+
+    public float getBulletSize()
+    {
+        return bullet_size;
+    }
+
+    void GameOver()
+    {
+        Destroy(gameObject);
+        FindObjectOfType<GameManager>().EndGame();
+    }
 }
