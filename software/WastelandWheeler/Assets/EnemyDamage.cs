7fb126f5330a1e047770c571951512597eeed5d6
Lasrrr <larsonpaul@live.ca>
2019-10-01 22:08:29 -0600

    Added Enemy, brought in from tutorial

---
+++ software/WastelandWheeler/Assets/EnemyDamage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
---
+++ software/WastelandWheeler/Assets/EnemyMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MovementBase
+{
+
+    public override Vector2 getDirection()
+    {
+        Vector2 mypos = gameObject.transform.position;
+        GameObject player = GameObject.FindWithTag("Player");
+        if (player != null)
+        {
+            Vector2 ppos = player.transform.position;
+            return (ppos - mypos).normalized;
+        }
+        return new Vector2(0, 0);
+    }
+
+}
