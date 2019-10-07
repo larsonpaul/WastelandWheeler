3d8094cdcfa6cd2502d4dc0b40d2d32444aea822
AJ <aj.nargang@gmail.com>
2019-10-06 21:13:09 -0600

    Remove Player_movement_top and change Top_game scene
    Put the changes from player_movement_top into player_movement and change the scene accordingly.

--- software/WastelandWheeler/Assets/Scripts/player_movement.cs
+++ software/WastelandWheeler/Assets/Scripts/player_movement.cs
@@ -9,6 +9,7 @@ public class player_movement : MonoBehaviour
 
     private static Player_stats stats;
     private float move_speed;
+    public Sprite img1, img2, img3, img4, img5, img6, img7, img8;
 
     Vector2 movement;
     Vector2 mouse_position;
@@ -48,6 +49,46 @@ public class player_movement : MonoBehaviour
         float angle = Mathf.Atan2(look_direction.y, look_direction.x) * Mathf.Rad2Deg - 90f;
 
         r_body.rotation= angle;
+
+        if (r_body.rotation > -70 && r_body.rotation < 0)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = img5;
+            this.GetComponent<SpriteRenderer>().flipY = false;
+        }
+
+        if (r_body.rotation < -70 && r_body.rotation > -110)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = img3;
+            this.GetComponent<SpriteRenderer>().flipY = true;
+            this.GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+        if (r_body.rotation < -110 && r_body.rotation > -160)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = img2;
+            //this.GetComponent<SpriteRenderer>().flipY = true;
+            //this.GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+        if (r_body.rotation < -160 && r_body.rotation > -200)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = img1;
+            this.GetComponent<SpriteRenderer>().flipY = true;
+        }
+
+        if (r_body.rotation < -200 && r_body.rotation > -250)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = img8;
+            //this.GetComponent<SpriteRenderer>().flipY = true;
+            //this.GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+        if (r_body.rotation < -250 && r_body.rotation > -270)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = img7;
+            this.GetComponent<SpriteRenderer>().flipY = true;
+            this.GetComponent<SpriteRenderer>().flipX = true;
+        }
 
     }
 }
