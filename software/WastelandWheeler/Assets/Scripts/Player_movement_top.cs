c55143a882baa9e3464ff065fb04113d6bd02873
AJ <aj.nargang@gmail.com>
2019-10-06 21:06:15 -0600

    Sprite turning
    Add to player_movement_top a place for the 8 images that will be used as the character turns in a circle. Change for each sprite is based on the Rigidbody rotation.

--- software/WastelandWheeler/Assets/Scripts/Player_movement_top.cs
+++ software/WastelandWheeler/Assets/Scripts/Player_movement_top.cs
@@ -9,7 +9,7 @@ public class Player_movement_top : MonoBehaviour
     public Camera cam;
     Vector2 movement;
     Vector2 mouse_position;
-    public Sprite[] player_sprites;
+    public Sprite img1, img2, img3, img4, img5, img6, img7, img8;
 
     // Update is called once per frame
     void Update()
@@ -35,17 +35,48 @@ public class Player_movement_top : MonoBehaviour
         float angle = Mathf.Atan2(look_direction.y, look_direction.x) * Mathf.Rad2Deg - 90f;
 
         // Set rotation to calculated angle
-        r_body.rotation= angle;
+        r_body.rotation = angle;
+     
+        if (r_body.rotation > -70 && r_body.rotation < 0)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = img5;
+            this.GetComponent<SpriteRenderer>().flipY = false;
+        }
 
-        if (r_body.rotation > -90 && r_body.rotation < 90)
+        if (r_body.rotation < -70 && r_body.rotation > -110)
         {
-            GetComponent<SpriteRenderer>().sprite = player_sprites[1];
-            Debug.Log("Front");
+            this.GetComponent<SpriteRenderer>().sprite = img3;
+            this.GetComponent<SpriteRenderer>().flipY = true;
+            this.GetComponent<SpriteRenderer>().flipX = true;
         }
-        if (r_body.rotation < -270 && r_body.rotation > -90)
+
+        if (r_body.rotation < -110  && r_body.rotation > -160)
         {
-            GetComponent<SpriteRenderer>().sprite = player_sprites[0];
-            Debug.Log("Back");
+            this.GetComponent<SpriteRenderer>().sprite = img2;
+            //this.GetComponent<SpriteRenderer>().flipY = true;
+            //this.GetComponent<SpriteRenderer>().flipX = true;
         }
+
+        if (r_body.rotation < -160 && r_body.rotation > -200)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = img1;
+            this.GetComponent<SpriteRenderer>().flipY = true;
+        }
+
+        if (r_body.rotation < -200 && r_body.rotation > -250)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = img8;
+            //this.GetComponent<SpriteRenderer>().flipY = true;
+            //this.GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+        if (r_body.rotation < -250 && r_body.rotation > -270)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = img7;
+            this.GetComponent<SpriteRenderer>().flipY = true;
+            this.GetComponent<SpriteRenderer>().flipX = true;
+        }
+
     }
+   
 }
