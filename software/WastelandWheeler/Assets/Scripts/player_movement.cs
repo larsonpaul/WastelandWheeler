fd8eb41ce47f5fdbe4fd4116dd2e1cc530f1a48e
AJ <aj.nargang@gmail.com>
2019-10-10 14:13:40 -0600

    Add Two Sprites to Char & finish rotation script
    Add the final two sprites to the player object and update the player_movement script to work with all 8 sprites.

--- software/WastelandWheeler/Assets/Scripts/player_movement.cs
+++ software/WastelandWheeler/Assets/Scripts/player_movement.cs
@@ -50,37 +50,24 @@ public class player_movement : MonoBehaviour
 
         r_body.rotation= angle;
 
-        if (r_body.rotation > -70 && r_body.rotation < 0)
+        if (r_body.rotation > -20 && r_body.rotation < 20)
         {
             this.GetComponent<SpriteRenderer>().sprite = img5;
             this.GetComponent<SpriteRenderer>().flipY = false;
         }
 
-        if (r_body.rotation < -70 && r_body.rotation > -110)
+        if (r_body.rotation > 20 && r_body.rotation < 65)
         {
-            this.GetComponent<SpriteRenderer>().sprite = img3;
+            this.GetComponent<SpriteRenderer>().sprite = img6;
+            this.GetComponent<SpriteRenderer>().flipY = false;
+            this.GetComponent<SpriteRenderer>().flipX = false;
+        }
+
+        if (r_body.rotation < 90 && r_body.rotation > 65)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = img7;
             this.GetComponent<SpriteRenderer>().flipY = true;
             this.GetComponent<SpriteRenderer>().flipX = true;
-        }
-
-        if (r_body.rotation < -110 && r_body.rotation > -160)
-        {
-            this.GetComponent<SpriteRenderer>().sprite = img2;
-            //this.GetComponent<SpriteRenderer>().flipY = true;
-            //this.GetComponent<SpriteRenderer>().flipX = true;
-        }
-
-        if (r_body.rotation < -160 && r_body.rotation > -200)
-        {
-            this.GetComponent<SpriteRenderer>().sprite = img1;
-            this.GetComponent<SpriteRenderer>().flipY = true;
-        }
-
-        if (r_body.rotation < -200 && r_body.rotation > -250)
-        {
-            this.GetComponent<SpriteRenderer>().sprite = img8;
-            //this.GetComponent<SpriteRenderer>().flipY = true;
-            //this.GetComponent<SpriteRenderer>().flipX = true;
         }
 
         if (r_body.rotation < -250 && r_body.rotation > -270)
@@ -88,7 +75,43 @@ public class player_movement : MonoBehaviour
             this.GetComponent<SpriteRenderer>().sprite = img7;
             this.GetComponent<SpriteRenderer>().flipY = true;
             this.GetComponent<SpriteRenderer>().flipX = true;
+
         }
+
+        if (r_body.rotation < -205 && r_body.rotation > -250)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = img8;
+            //this.GetComponent<SpriteRenderer>().flipY = true;
+            //this.GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+        if (r_body.rotation < -160 && r_body.rotation > -205)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = img1;
+            this.GetComponent<SpriteRenderer>().flipY = true;
+        }
+
+        if (r_body.rotation < -110 && r_body.rotation > -160)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = img2;
+            //this.GetComponent<SpriteRenderer>().flipY = true;
+            this.GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+        if (r_body.rotation < -65 && r_body.rotation > -110)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = img3;
+            this.GetComponent<SpriteRenderer>().flipY = true;
+            this.GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+        if (r_body.rotation < -20 && r_body.rotation > -65)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = img4;
+            this.GetComponent<SpriteRenderer>().flipY = false;
+            this.GetComponent<SpriteRenderer>().flipX = false;
+        }
+
 
     }
 }
