4569167c41a9daac2cd5cb9b9ee43a94f6852842
AJ <aj.nargang@gmail.com>
2019-10-16 19:39:42 -0600

    Add an Adrenaline Bar
    Add a smaller health bar below the current as an adrenaline bar. No functionality at this time.

---
+++ software/WastelandWheeler/Assets/Scripts/AdrenalineBar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdrenalineBar : MonoBehaviour
+{
+    private Transform bar;
+
+    void Start()
+    {
+        bar = transform.Find("Bar");
+    }
+
+    public void SetScale(float scale)
+    {
+        bar.localScale = new Vector3(Mathf.Min(Mathf.Max(scale, 0), 1), 1f);
+    }
+}
