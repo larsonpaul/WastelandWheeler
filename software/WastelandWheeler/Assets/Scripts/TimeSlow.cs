09561c7668e694b91f70a621a3265c5e1b5c081c
AJ <aj.nargang@gmail.com>
2019-10-16 19:41:03 -0600

    Add a TimeSlow Button
    On leftshift hold, reduce the time scale to .5 until it is released.

---
+++ software/WastelandWheeler/Assets/Scripts/TimeSlow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeSlow : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Time.timeScale = .5f;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            Time.timeScale = 1f;
+        }
+    }
+    
+}
