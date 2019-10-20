5a02c641af3953c3b42948282c24fbc4265b9886
Jadyn K <jadyn.kimber@gmail.com>
2019-10-19 22:18:18 -0600

    added forced move frame object and script

---
+++ software/Scripts/FrameMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameMove : MonoBehaviour
+{
+    
+
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        gameObject.transform.position = new Vector3(transform.position.x+ 0.05f, transform.position.y, transform.position.z);
+    }
+}
