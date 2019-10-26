88c8b79e13d184548a794cc9ebd56e82f34e405f
Jesse <jfb439@mail.usask.ca>
2019-10-25 21:42:40 -0600

    added a bunch of things for creating a scene transtition

---
+++ software/WastelandWheeler/Assets/Scripts/SceneChange/LevelChanger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+    public Animator animator;
+
+    private string levelToLoad;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("l"))
+        {
+            FadeToPlatformer("Level1_2D");
+        }
+    }
+
+    public void FadeToPlatformer(string levelName)
+    {
+        animator.SetTrigger("T2PSceneChange");
+        levelToLoad = levelName;
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
