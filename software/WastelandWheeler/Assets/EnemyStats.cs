416eee6c0f36e4eaec05fd177b5588f6108b364b
Rachael Kenny <rachaelkenny@rachaels-air.gateway.sasktel.net>
2019-10-30 12:34:08 -0600

    created very basic scripts for enemyStats and an alternate player bullet and alternate player bullet

---
+++ software/WastelandWheeler/Assets/EnemyStats.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+
+    public float healthMax = 20f;
+    public float healthCurrent = 20f;
+    public float shields = 1;
+    private GameObject dda;
+
+
+    private void Start()
+    {
+        //dda = GameObject.Find("DDA");
+        //dda.GetComponent<DynamicDifficultyAdjuster>().Subscribe(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void RemoveHealth(float num)
+    {
+        if (num <= 0)
+        {
+            if (num < 0)
+            {
+                string error = gameObject.name + ".RemoveHealth() given negative float " + num;
+                Debug.LogError(error);
+            }
+            return;
+        }
+        else
+        {
+            healthCurrent -= num;
+            if (healthCurrent <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    // Function to grab the current health of the player
+    public float GetHealth()
+    {
+        return healthCurrent;
+    }
+}
+
+
---
+++ software/WastelandWheeler/Assets/damageBullet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class damageBullet : MonoBehaviour
+{
+    //public GameObject hit_effect;
+    public float damage;
+
+    // Start is called before the first frame update, get the player's stats
+    void Start()
+    {
+       
+    }
+
+    // on collision deal damage
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //GameObject effect = Instantiate(hit_effect, transform.position, Quaternion.identity);
+        //Destroy(effect, 5f);
+        GameObject obj = collision.gameObject;
+
+        // cases where bullet is not destroyed
+        string[] tags = { "Player", "Shot", "Power_Up" };
+        for (int i = 0; i < tags.Length; i++)
+        {
+            if (obj.CompareTag(tags[i])) return;
+        }
+        // cases where bullet is destroyed
+        Destroy(gameObject);
+        if (obj.CompareTag("Enemy"))
+        {
+            obj.GetComponent<EnemyStats>().RemoveHealth(damage);
+            return;
+        }
+    }
+}
