d55ab03392263296548e6b9f5f81e482b0abb72c
AJ <aj.nargang@gmail.com>
2019-10-30 21:58:59 -0600

    Full Upgrade Screen
    Add an upgrade screen for future development.

---
+++ software/WastelandWheeler/Assets/UpgradeMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UpgradeMenu : MonoBehaviour
+{
+    public void StartNextLevel()
+    {
+        // If scene was topdown, load platformer
+        SceneManager.LoadScene(2);
+        Time.timeScale = 1f;
+        PauseMenu.GameIsPaused = false;
+
+        // Else load topdown
+        /* Don't currently know how to check previous scene */
+    }
+
+}
