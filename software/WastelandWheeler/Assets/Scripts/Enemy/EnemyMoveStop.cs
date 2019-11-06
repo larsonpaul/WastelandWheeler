7b8765d626ae489d50f7af6a0650820e29d886fd
Jadyn K <jadyn.kimber@gmail.com>
2019-11-06 17:05:22 -0600

    added movement for some of the shooting enemies

---
+++ software/WastelandWheeler/Assets/Scripts/Enemy/EnemyMoveStop.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMoveStop : MonoBehaviour
+{
+    private Rigidbody2D rbody;
+    private EnemyStats stats;
+
+    private Vector2 dir;
+    private float dist;
+
+    public float range = 5.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        stats = GetComponent<EnemyStats>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dir = getDirection();
+        dist = getDistance();
+
+        if (dist > range)
+        {
+            rbody.AddForce(new Vector2(stats.speed * dir.x, stats.speed * dir.y));
+        }
+            
+          
+    }
+
+    Vector2 getDirection()
+    {
+        Vector2 mypos = gameObject.transform.position;
+        GameObject player = GameObject.FindWithTag("Player");
+        if (player != null)
+        {
+            Vector2 ppos = player.transform.position;
+            return (ppos - mypos).normalized;
+        }
+        return new Vector2(0, 0);
+    }
+
+    float getDistance()
+    {
+        Vector2 mypos = gameObject.transform.position;
+        GameObject player = GameObject.FindWithTag("Player");
+        if (player != null)
+        {
+            Vector2 ppos = player.transform.position;
+            return Vector2.Distance(mypos, ppos);
+        }
+        return -1.0f;
+    }
+}
