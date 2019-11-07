a4be4007226a4c926bf3e2067b7425b8186ce913
Jadyn K <jadyn.kimber@gmail.com>
2019-11-06 22:49:58 -0600

    added new enemy bullet behaviour, fixed bullet collisions

--- software/WastelandWheeler/Assets/Scripts/Enemy/EnemyStats.cs
+++ software/WastelandWheeler/Assets/Scripts/Enemy/EnemyStats.cs
@@ -17,7 +17,7 @@ public class EnemyStats : MonoBehaviour, IDiffcultyAdjuster, ICreatureStats
     public float firerate;
     public float baseFirerate = 0;
 
-    public float damage = 10;
+    public float damage = 10f;
     public float baseDamage = 10;
 
     private EnemyBar healthBar;
