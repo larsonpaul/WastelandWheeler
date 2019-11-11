45863e2d37481da9a094276349238f6fb3dcdafe
AJ <aj.nargang@gmail.com>
2019-11-11 14:00:08 -0600

    Rework Upgrade Button Prefabs
    Add an UpgradeButtons script that will adjust the players stats.
    Rework the UpdateValues script to update the numbers above and below the buttons.

--- software/WastelandWheeler/Assets/Scripts/UpdateValues.cs
+++ software/WastelandWheeler/Assets/Scripts/UpdateValues.cs
@@ -1,23 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UpdateValues : MonoBehaviour
 {
     private Player_stats stats = Player_stats.Instance;
+    private float currCoins;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    TextMeshProUGUI oldHealth;
+    [SerializeField]
+    TextMeshProUGUI oldSpeed;
+    [SerializeField]
+    TextMeshProUGUI oldROF;
+    [SerializeField]
+    TextMeshProUGUI oldDamage;
+    [SerializeField]
+    TextMeshProUGUI oldBullets;
+    [SerializeField]
+    TextMeshProUGUI newHealth;
+    [SerializeField]
+    TextMeshProUGUI newSpeed;
+    [SerializeField]
+    TextMeshProUGUI newROF;
+    [SerializeField]
+    TextMeshProUGUI newDamage;
+    [SerializeField]
+    TextMeshProUGUI newBullets;
+    [SerializeField]
+    TextMeshProUGUI tokensLeft;
+
+    private void Start()
     {
+        oldHealth.text = stats.GetMaxHealth().ToString();
 
+        oldSpeed.text = stats.GetSpeed().ToString();
+
+        oldROF.text = stats.GetROF().ToString();
+
+        oldDamage.text = stats.GetDamage().ToString();
+
+        oldBullets.text = stats.GetBulletSize().ToString();
     }
-
-    // Update is called once per frame
     void Update()
-    { 
-        // if the user hits a button with tag button
-        // reduce tokens by 1
-        // add one value to the pressed button
-        
+    {
+        currCoins = stats.GetCoins();
+
+        tokensLeft.text = "Tokens: " + currCoins.ToString();
+
+        newHealth.text = stats.GetMaxHealth().ToString();
+
+        newSpeed.text = stats.GetSpeed().ToString();
+
+        newROF.text = stats.GetROF().ToString();
+
+        newDamage.text = stats.GetDamage().ToString();
+
+        newBullets.text = stats.GetBulletSize().ToString();
     }
 }
