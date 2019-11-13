548832941726bb532ab969fc04a98d4df9b1e340
AJ <aj.nargang@gmail.com>
2019-11-12 22:58:34 -0600

    Add Larger Background Music File
    Add an obbject that won't destroy on load and play music for 25 minutes before looping.

---
+++ software/WastelandWheeler/Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    public static DontDestroy Instance
+    {
+        get;
+        set;
+    }
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
