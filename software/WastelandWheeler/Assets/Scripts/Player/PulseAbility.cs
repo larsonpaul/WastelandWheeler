6e4d6cf53f8422ee376cfb724de8044e6e5b0300
Lasrrr <larsonpaul@live.ca>
2019-11-21 03:43:29 -0600

    Added new ability on spacebar which destroys enemy bullets and throws enemies back

---
+++ software/WastelandWheeler/Assets/Scripts/Player/PulseAbility.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PulseAbility : MonoBehaviour
+{
+    private Player_stats stats;
+
+    private PulseCollider collider;
+
+    private float cost = 50.0f;
+
+    private bool onCooldown = false;
+    private float cooldownLength = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stats = gameObject.GetComponent<Player_stats>();
+        collider = transform.Find("Pulse").gameObject.GetComponent<PulseCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && stats.GetAdrenaline() >= cost && !onCooldown)
+        {
+            onCooldown = true;
+
+            // functionality
+            collider.Activate();
+
+            // cost and cooldown
+            stats.RemoveAdrenaline(cost);
+            Invoke("Cooldown", cooldownLength);
+        }
+    }
+
+    private void Cooldown()
+    {
+        onCooldown = false;
+    }
+}
---
+++ software/WastelandWheeler/Assets/Scripts/Player/PulseCollider.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulseCollider : MonoBehaviour
+{
+    private Vector3 ppos;
+
+    [SerializeField]
+    private float power = 20;
+
+    private const float MINSCALE = 2.0f;
+    private const float MULTIPLIER = 4.0f;
+
+    private float pulseDuration = .25f;
+
+    // Start is called before the first frame update
+    //void Awake()
+    //{
+    //    ppos = transform.parent.position;
+    //}
+
+    void FixedUpdate()
+    {
+        float newScale = transform.localScale.x + (MaxScale() / pulseDuration) * Time.fixedDeltaTime;
+
+        if (newScale > MaxScale())
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+
+        transform.localScale = new Vector3(newScale, newScale);
+    }
+
+    public void Activate()
+    {
+        gameObject.SetActive(true);
+        transform.localScale = new Vector3(MINSCALE, MINSCALE);
+    }
+
+    private float MaxScale()
+    {
+        return MINSCALE * MULTIPLIER;
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        GameObject obj = col.gameObject;
+        if (obj.CompareTag("Enemy"))
+        {
+            Debug.Log("Collision with " + col.gameObject.name);
+            Rigidbody2D enemyRB = obj.GetComponent<Rigidbody2D>();
+            Vector3 ppos = transform.parent.position;
+
+            Debug.Log("Enemy  " + obj.transform.position);
+            Debug.Log("Player " + ppos);
+            Vector2 force = (obj.transform.position - ppos).normalized * power;
+
+            enemyRB.AddForce(force);
+        }
+        else if (obj.CompareTag("bullet"))
+        {
+            Destroy(obj);
+        }
+    }
+}
