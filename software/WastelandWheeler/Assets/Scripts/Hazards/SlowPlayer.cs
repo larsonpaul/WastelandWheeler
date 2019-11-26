b58544dce67630b83718293f7f7f611868c9d53e
AJ <aj.nargang@gmail.com>
2019-11-26 16:33:01 -0600

    Create a Slowing Effect for Cracks in Map
    Add a script which will slow the player's speed by half whenever he goes over a crack.

---
+++ software/WastelandWheeler/Assets/Scripts/Hazards/SlowPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowPlayer : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.GetComponent<Player_stats>().move_speed *= .5f;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        collision.GetComponent<Player_stats>().move_speed = collision.GetComponent<Player_stats>().baseSpeed;
+    }
+}
