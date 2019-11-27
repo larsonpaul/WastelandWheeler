05b204a1980a4f0dbad84e83640ecb01a70e6762
AJ <aj.nargang@gmail.com>
2019-11-27 14:16:26 -0600

    Fix Collisions With SlowArea
    Check for tags to not affect anything but the player (for now).

--- software/WastelandWheeler/Assets/Scripts/Hazards/SlowPlayer.cs
+++ software/WastelandWheeler/Assets/Scripts/Hazards/SlowPlayer.cs
@@ -6,11 +6,26 @@ public class SlowPlayer : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        collision.GetComponent<Player_stats>().move_speed *= .5f;
+        if(collision.CompareTag("Enemy") || collision.CompareTag("bullet") || collision.CompareTag("Shot") || collision.CompareTag("Magnet") || collision.CompareTag("Power_Up"))
+        {
+            return;
+        }
+        else
+        {
+            collision.GetComponent<Player_stats>().move_speed *= .5f;
+        }
+        
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        collision.GetComponent<Player_stats>().move_speed = collision.GetComponent<Player_stats>().baseSpeed;
+        if (collision.CompareTag("Enemy") || collision.CompareTag("bullet") || collision.CompareTag("Shot") || collision.CompareTag("Magnet") || collision.CompareTag("Power_Up"))
+        {
+            return;
+        }
+        else
+        {
+            collision.GetComponent<Player_stats>().move_speed = collision.GetComponent<Player_stats>().baseSpeed;
+        }
     }
 }
