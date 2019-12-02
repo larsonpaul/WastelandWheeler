9d69c447f674655939ea691a4779fd18c52172c4
Lasrrr <larsonpaul@live.ca>
2019-12-01 19:40:14 -0600

    Fixed stacking slows and enemy bullet collisions

--- software/WastelandWheeler/Assets/Scripts/Hazards/SlowPlayer.cs
+++ software/WastelandWheeler/Assets/Scripts/Hazards/SlowPlayer.cs
@@ -4,28 +4,27 @@ using UnityEngine;
 
 public class SlowPlayer : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    private static int sem = 0;
+
+    private void OnTriggerEnter2D(Collider2D col)
     {
-        if(collision.CompareTag("Enemy") || collision.CompareTag("bullet") || collision.CompareTag("Shot") || collision.CompareTag("Magnet") || collision.CompareTag("Power_Up"))
+        if(col.CompareTag("Player"))
         {
-            return;
+            if (sem++ == 0)
+            {
+                col.GetComponent<Player_stats>().move_speed *= .5f;
+            }
         }
-        else
-        {
-            collision.GetComponent<Player_stats>().move_speed *= .5f;
-        }
-        
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+    private void OnTriggerExit2D(Collider2D col)
     {
-        if (collision.CompareTag("Enemy") || collision.CompareTag("bullet") || collision.CompareTag("Shot") || collision.CompareTag("Magnet") || collision.CompareTag("Power_Up"))
+        if (col.CompareTag("Player"))
         {
-            return;
-        }
-        else
-        {
-            collision.GetComponent<Player_stats>().move_speed *= 2f;
+            if (--sem == 0)
+            {
+                col.GetComponent<Player_stats>().move_speed *= 2f;
+            }
         }
     }
 }
