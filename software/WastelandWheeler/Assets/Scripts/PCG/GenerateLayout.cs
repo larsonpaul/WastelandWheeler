e39ee735a7abbdcdf971a5f1b484302374d52cea
Jesse <jfb439@mail.usask.ca>
2019-12-02 12:46:29 -0600

    starting actual development of script to creat randomized level layouts

--- software/WastelandWheeler/Assets/Scripts/PCG/GenerateLayout.cs
+++ software/WastelandWheeler/Assets/Scripts/PCG/GenerateLayout.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class GenerateLayout : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // the transform limits of the area where obstacles can be placed 
+    // these numbers have been chosen to leave a corridor around the outer edge 
+    // of the environment so that there is always a path through
+    public int lowerX = -10;
+    public int upperX = 9;
+    public int lowerY = 12;
+    public int upperY = 302;
+
+    // values that control how many obstacles are placed in the environment 
+    // the script will spawn a random number within theses bounds
+    public int minObstacles = 30;
+    public int maxObstacles = 40;
+
+    //container to hold obstacle prefabs
+    public GameObject[] obstacles;
+
+    private Spawn_Manager spawnManager;
     void Start()
     {
-        
+        spawnManager = FindObjectOfType<Spawn_Manager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SpawnObstacles()
     {
-        
+
     }
 }
