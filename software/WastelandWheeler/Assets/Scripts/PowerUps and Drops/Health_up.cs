2f90298d05e8b0a9c195ef5ea4474a721fe972d9
Jesse <jfb439@mail.usask.ca>
2019-12-04 13:06:08 -0600

    started script that will let us display DDA changes to the game, also tweaked a couple things with player_stats and health_up

--- software/WastelandWheeler/Assets/Scripts/PowerUps and Drops/Health_up.cs
+++ software/WastelandWheeler/Assets/Scripts/PowerUps and Drops/Health_up.cs
@@ -7,20 +7,26 @@ using UnityEngine;
  */
 public class Health_up : MonoBehaviour
 {
-    public float heal = 0.20f;
+    public float baseHeal = 0.20f;
     private static Player_stats stats;
 
     private bool used = false;
+
+    private DynamicDifficultyAdjuster dda;
 
     // Start is called before the first frame update, get the player's stats
     void Start()
     {
         stats = GameObject.FindWithTag("Player").GetComponent<Player_stats>();
+        dda = DynamicDifficultyAdjuster.Instance;
     }
 
     // On collision, check for player tag and add health
     private void OnTriggerEnter2D(Collider2D col)
     {
+        int difficulty = dda.GetDifficulty();
+        float heal = baseHeal *(1.0f - (0.01f * difficulty));
+
         if (col.CompareTag("Player") && used == false)
         {
             used = true;
