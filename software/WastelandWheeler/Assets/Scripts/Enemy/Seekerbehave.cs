5e9da53924b4a1a6435170cadd129483a984ad89
Lasrrr <larsonpaul@live.ca>
2019-12-04 20:04:24 -0600

    Fixed seekers

--- software/WastelandWheeler/Assets/Scripts/Enemy/Seekerbehave.cs
+++ software/WastelandWheeler/Assets/Scripts/Enemy/Seekerbehave.cs
@@ -33,34 +33,34 @@ public class Seekerbehave : MonoBehaviour
         }
         else
         {
-            int shots = 10;
+            //int shots = 10;
 
-            float angle = 360f / shots;
+            //float angle = 360f / shots;
 
-            for (float i=0f; i < 360; i += angle)
-            {
-                Quaternion q = Quaternion.AngleAxis(i, Vector3.up);
-                Debug.Log(q.ToString());
-                GameObject projectile = (GameObject)Instantiate(bullet, transform.position, q);
-                projectile.GetComponent<Rigidbody2D>().AddForce(projectile.transform.up * speed);
-            }
+            //for (float i=0f; i < 360; i += angle)
+            //{
+            //    Quaternion q = Quaternion.AngleAxis(i, Vector3.up);
+            //    Debug.Log(q.ToString());
+            //    GameObject projectile = (GameObject)Instantiate(bullet, transform.position, q);
+            //    projectile.GetComponent<Rigidbody2D>().AddForce(projectile.transform.up * speed);
+            //}
 
-            //GameObject projectile = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
-            //projectile.GetComponent<Rigidbody2D>().AddForce(new Vector2(0.7f* speed, 0.7f * speed));
-            //GameObject projectile2 = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
-            //projectile2.GetComponent<Rigidbody2D>().AddForce(new Vector2(-0.7f * speed, 0.7f * speed));
-            //GameObject projectile3 = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
-            //projectile3.GetComponent<Rigidbody2D>().AddForce(new Vector2(0.7f * speed, -0.7f * speed));
-            //GameObject projectile4 = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
-            //projectile4.GetComponent<Rigidbody2D>().AddForce(new Vector2(-0.7f * speed, -0.7f * speed));
-            //GameObject projectile5 = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
-            //projectile5.GetComponent<Rigidbody2D>().AddForce(new Vector2(0 * speed, 1 * speed));
-            //GameObject projectile6 = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
-            //projectile6.GetComponent<Rigidbody2D>().AddForce(new Vector2(-1 * speed, 0 * speed));
-            //GameObject projectile7 = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
-            //projectile7.GetComponent<Rigidbody2D>().AddForce(new Vector2(0 * speed, -1 * speed));
-            //GameObject projectile8 = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
-            //projectile8.GetComponent<Rigidbody2D>().AddForce(new Vector2(1 * speed, 0 * speed));
+            GameObject projectile = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
+            projectile.GetComponent<Rigidbody2D>().AddForce(new Vector2(0.7f * speed, 0.7f * speed));
+            GameObject projectile2 = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
+            projectile2.GetComponent<Rigidbody2D>().AddForce(new Vector2(-0.7f * speed, 0.7f * speed));
+            GameObject projectile3 = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
+            projectile3.GetComponent<Rigidbody2D>().AddForce(new Vector2(0.7f * speed, -0.7f * speed));
+            GameObject projectile4 = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
+            projectile4.GetComponent<Rigidbody2D>().AddForce(new Vector2(-0.7f * speed, -0.7f * speed));
+            GameObject projectile5 = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
+            projectile5.GetComponent<Rigidbody2D>().AddForce(new Vector2(0 * speed, 1 * speed));
+            GameObject projectile6 = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
+            projectile6.GetComponent<Rigidbody2D>().AddForce(new Vector2(-1 * speed, 0 * speed));
+            GameObject projectile7 = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
+            projectile7.GetComponent<Rigidbody2D>().AddForce(new Vector2(0 * speed, -1 * speed));
+            GameObject projectile8 = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
+            projectile8.GetComponent<Rigidbody2D>().AddForce(new Vector2(1 * speed, 0 * speed));
 
             stats.OnDeath();
         }
